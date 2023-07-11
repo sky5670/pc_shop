25157a3a3e4db0e7d3187c5a057f91457284ae37
Javier Perez de arce <skaip.5670@gmail.com>
2023-07-10 20:13:26 -0400

    tercer comit

---
+++ src/app/crud/crud-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'product-list', pathMatch: 'full'},
+  {path: 'product-list', component: ProductListComponent},
+  {path: 'create-product', component: ProductFormComponent},
+  {path: 'update-product/:productId', component: ProductFormComponent},
+  {path: 'view-product-details/:productId', component: ProductDetailsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CRUDRoutingModule { }
